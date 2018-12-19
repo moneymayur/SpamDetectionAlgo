ec9daaa4bc629fcb66f56e47cedb8edc5e53cd9e
Mayur Mathur <moneymayur@users.noreply.github.com>
2018-12-19 15:36:54 +0530

    Create wordDictionary.py

---
+++ wordDictionary.py
@@ -0,0 +1,19 @@
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
+    all_words = []       
+    for mail in emails:    
+        with open(mail) as m:
+            for i,line in enumerate(m):
+                if i == 2:  #Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+    
+  dictionary = Counter(all_words)
+  list_to_remove = dictionary.keys()
+  for item in list_to_remove:
+    if item.isalpha() == False: 
+        del dictionary[item]
+    elif len(item) == 1:
+        del dictionary[item]
+  dictionary = dictionary.most_common(3000)
+    return dictionary
